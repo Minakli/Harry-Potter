8878de4d59c7546c18bc2592c0c8c58901c0e4b6
Minakli <Ges-a@ya.ru>
2022-12-08 10:50:56 +0300

    created filter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
+import { data } from "./data.js";
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const headForm = document.createElement("form");
-//Отрисовка header
+const inputName = document.createElement("input");
+const mainContainer = document.createElement("div");
+const selectSchool = document.createElement("select");
+//Отрисовывает header
 function createHeader() {
   const headerContainer = document.createElement("div");
   const h1 = document.createElement("h1");
   const h2 = document.createElement("h2");
   const labelName = document.createElement("label");
   const labelSchool = document.createElement("label");
-  const inputName = document.createElement("input");
-  const selectSchool = document.createElement("select");
   const decorLine = document.createElement("div");
-  const optSchool_1 = document.createElement("option");
-  const optSchool_2 = document.createElement("option");
-  const optSchool_3 = document.createElement("option");
-  const optSchool_4 = document.createElement("option");
+  const optSchool_0 = document.createElement("option");
+  const optSchool_all = document.createElement("option");
   headerContainer.classList.add("header__container");
   h1.textContent = "Harry Potter";
   h2.textContent = "View all characters from the Harry Potter universe";
@@ -24,34 +24,110 @@ function createHeader() {
   labelSchool.textContent = "School";
   inputName.setAttribute("type", "text");
   inputName.setAttribute("id", "input__name");
-  inputName.setAttribute("placeholder", "Hermione");
+  inputName.setAttribute("placeholder", "   Select unit");
   selectSchool.setAttribute("type", "text");
   selectSchool.setAttribute("id", "input__school");
-  optSchool_1.setAttribute("disabled", "disabled");
-  optSchool_1.setAttribute("selected", "selected");
-  optSchool_1.textContent = "Choose one";
-  optSchool_2.textContent = "sect";
-  optSchool_3.textContent = "sect";
-  optSchool_4.textContent = "sect";
+  optSchool_0.setAttribute("disabled", "disabled");
+  optSchool_0.setAttribute("selected", "selected");
+  optSchool_0.textContent = "Choose one";
+  optSchool_all.textContent = "Select all";
   decorLine.classList.add("header__decorline");
   header.append(headerContainer);
-  headerContainer.append(h1);
-  headerContainer.append(h2);
-  headerContainer.append(headForm);
+  headerContainer.append(h1, h2, headForm);
   header.append(decorLine);
-  headForm.append(labelName);
-  headForm.append(labelSchool);
-  labelName.append(inputName);
-  labelSchool.append(selectSchool);
-  selectSchool.append(optSchool_1);
-  selectSchool.append(optSchool_2);
-  selectSchool.append(optSchool_3);
-  selectSchool.append(optSchool_4);
+  headForm.append(labelName, labelSchool, inputName, selectSchool);
+  selectSchool.prepend(optSchool_all);
+  selectSchool.prepend(optSchool_0);
 }
 
-//Отрисовка main
+//Создает массив школ из полученных карт и вызывает функцию отрисовки списка
+createHouses();
+function createHouses() {
+  let allHouses = data.map((elem) => {
+    return elem.house;
+  });
+  let houses = [];
+  for (let elem of allHouses) {
+    if (houses.find((item) => item == elem)) {
+    } else {
+      houses.push(elem);
+      createOptionsList(elem);
+    }
+  }
+}
+//Создает список options из масства школ
+function createOptionsList(elem) {
+  let optionSchool = document.createElement("option");
+  if (elem) {
+    selectSchool.append(optionSchool);
+    optionSchool.textContent = elem;
+  }
+}
+//Отрисовка карточки
+function createCards(obj) {
+  mainContainer.classList.add("main__container");
+  const wrapper = document.createElement("div");
+  const img = document.createElement("img");
+  const title = document.createElement("p");
+  const actor = document.createElement("p");
+  const gender = document.createElement("p");
+  const house = document.createElement("p");
+  const wand = document.createElement("p");
+  const alive = document.createElement("p");
+  wrapper.classList.add("wrapper");
+  img.setAttribute("src", "https://via.placeholder.com/334x192");
+  img.setAttribute("alt", obj.name);
+  img.setAttribute("width", "334");
+  img.setAttribute("height", "192");
+  title.classList.add("name_hero");
+  title.textContent = obj.name;
+  actor.textContent = "Actor: " + obj.actor;
+  gender.textContent = "Gender: " + obj.gender;
+  if (obj.house) {
+    house.textContent = "House: " + obj.house;
+  } else {
+    house.textContent = "House: none";
+  }
+  wand.textContent = "Wand core: " + obj.wand.wood + obj.wand.core;
+  if (obj.alive) {
+    alive.textContent = "Alive: yes";
+  } else {
+    alive.textContent = "Alive: no";
+  }
+  main.append(mainContainer);
+  mainContainer.append(wrapper);
+  wrapper.append(img, title, actor, gender, house, wand, alive);
+}
+//Перебор и отрисовка массива карточек
+function showCards(arr) {
+  mainContainer.innerHTML = "";
+  arr.forEach((elem) => {
+    createCards(elem);
+  });
+}
+//Фильтрует массив и вызывает функцию отрисовки
+function createFilterDataArr() {
+  let dataPreFiltered = data.filter((elem) =>
+    elem.name.toLowerCase().includes(inputName.value.toLowerCase())
+  );
+  let dataFiltered;
+  if (
+    selectSchool.value !== "Choose one" &&
+    selectSchool.value !== "Select all"
+  ) {
+    dataFiltered = dataPreFiltered.filter(
+      (elem) => elem.house == selectSchool.value
+    );
+  } else {
+    dataFiltered = dataPreFiltered;
+  }
+  showCards(dataFiltered);
+}
 
 createHeader();
+showCards(data);
 headForm.addEventListener("submit", (event) => {
   event.preventDefault();
 });
+inputName.addEventListener("input", createFilterDataArr);
+selectSchool.addEventListener("change", createFilterDataArr);
